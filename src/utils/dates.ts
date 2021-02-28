c3f8c4b9f3959d6a2395db233bc6556cf679a741
HipsterBrown <headhipster@hipsterbrown.com>
2021-02-28 01:40:36 +0000

    Deploying to gh-pages from  @ 229a4fa8819797bf3ac198f823ceb1f61c1a1986 🚀

--- src/utils/dates.ts
+++
@@ -1,5 +0,0 @@
-export const formatDate = (date: string) =>
-  new Intl.DateTimeFormat("en-US", {
-    month: "long",
-    day: "numeric"
-  }).format(new Date(date));
--- src/utils/errors.ts
+++
@@ -1,4 +0,0 @@
-export const mapErrors = (errors: Record<string, string[]>): string[] =>
-  Object.keys(errors)
-    .map(key => errors[key].map(message => [key, message].join(" ")))
-    .flat();
